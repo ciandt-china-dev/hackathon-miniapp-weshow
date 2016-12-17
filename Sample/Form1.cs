72f2d224f920a758e9b22673d5f8c3931272e592
dingsongjie <377973147@qq.com>
2016-12-17 13:24:03 +0800

    add sample project

---
+++ Sample/Form1.cs
@@ -0,0 +1,40 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CascadeClassifier haar = new CascadeClassifier("haarcascade_eye.xml");    //初始化分类器
+            Image<Bgr, byte> frame = new Image<Bgr, byte>(@"1.jpg");
+            Rectangle[] results = haar.DetectMultiScale(frame, 1.3, 3, new System.Drawing.Size(10, 10));       //检测并将数据储存
+            foreach (Rectangle result in results)
+            {
+
+                CvInvoke.Rectangle(frame, result, new Bgr(Color.Red).MCvScalar, 2);  //在检测到的区域绘制红框
+                                                                                     //  Image<Bgr, Byte> imageGlass = new Image<Bgr, byte>(@"C:\Users\37797\Desktop\glass.png");
+                                                                                     //CvInvoke.BitwiseNot(frame, imageGlass, result)
+            }
+
+            //Image<Bgr, Byte> imageGlass= new Image<Bgr, byte>(@"C:\Users\37797\Desktop\glass.png");
+            //CvInvoke.BitwiseNot(frame, imageGlass,)
+            imageBox1.Image = frame;
+        }
+    }
+}
---
+++ WeShow/Controllers/ImageController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+
+namespace WeShow.Controllers
+{
+    public class ImageController : ApiController
+    {
+        // GET: Image
+        [HttpPost]
+        [Route("/api/image/automatic")]
+        public HttpResponseMessage Index()
+        {
+            //文件保存目录路径 
+            string SaveTempPath = "/imageTemp";
+            String dirTempPath = HttpContext.Current.Server.MapPath(SaveTempPath);
+            var provider = new MultipartFormDataStreamProvider(dirTempPath);
+            var task = Request.Content.ReadAsMultipartAsync(provider).ContinueWith(m => {
+                var file = provider.FileData[0];
+                
+                string orfilename = file.Headers.ContentDisposition.FileName.TrimStart('"').TrimEnd('"');
+                FileInfo fileinfo = new FileInfo(file.LocalFileName);
+                String ymd = DateTime.Now.ToString("yyyyMMdd", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+                String newFileName = DateTime.Now.ToString("yyyyMMddHHmmss_ffff", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+                string fileExt = orfilename.Substring(orfilename.LastIndexOf('.'));
+                fileinfo.CopyTo(Path.Combine(dirTempPath, newFileName + fileExt), true);
+                fileinfo.Delete();
+            });
+           
+            var imgPath = @"11";
+            //从图片中读取byte
+            var imgByte = File.ReadAllBytes(imgPath);
+            //从图片中读取流
+            var imgStream = new MemoryStream(File.ReadAllBytes(imgPath));
+            var resp = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(imgByte)
+                //或者
+                //Content = new StreamContent(stream)
+            };
+            resp.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpg");
+            return resp;
+        }
+    }
+}
