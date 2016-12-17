78073a83cdc6e90be2e82dd8ca5266d1693bc0e6
dingsongjie <377973147@qq.com>
2016-12-17 17:29:15 +0800

    update

--- WeShow/Controllers/ImageController.cs
+++ WeShow/Controllers/ImageController.cs
@@ -37,12 +37,19 @@ namespace WeShow.Controllers
             Image<Bgr, byte> frame = new Image<Bgr, byte>(serverImageRoot);  //加载上传的图片
 
             List<Rectangle> Rectangles = GetEyesByImage(haar, frame);
-
-
-            Image<Bgr, Byte> imageGlass = ChooseGlass(GetServerPath(serverImageRoot));
-            if (Rectangles.Count != 2)
+            foreach (var Rectangle in Rectangles)
             {
-                return new JsonResult(new SampleResult() { Status = 2, Data = "未能正确识别人眼" });
+
+            }
+
+            Image<Bgr, Byte> imageGlass = ChooseGlass(serverImageRoot);
+            if (Rectangles.Count < 2|| Rectangles.Count>3)
+            {
+                return new JsonResult(new SampleResult() { Status = 2, Data = "我现在还不够聪明，需要钱来改善我的基因" });
+            }
+            if(Rectangles.Count ==3)
+            {
+                ExecuteWhenRectangleCountEquals3(Rectangles);
             }
             SortRectangle(Rectangles);
             Bitmap imageResult = GetUpdatedImage(frame, Rectangles, imageGlass);
@@ -51,12 +58,39 @@ namespace WeShow.Controllers
             return new JsonResult(new SampleResult() { Status = 1, Data = Path.Combine("image", Path.Combine(imageRoot, file.FileName)) });
         }
 
+        private static void ExecuteWhenRectangleCountEquals3(List<Rectangle> Rectangles)
+        {
+            var eyewidth0 = Rectangles[0].Width;
+            var eyewidth1 = Rectangles[1].Width;
+            var eyewidth2 = Rectangles[2].Width;
+            List<int> betweenArry = new List<int>();
+            var between0and1 = Math.Abs(Rectangles[0].Width - Rectangles[1].Width);
+            betweenArry.Add(between0and1);
+            var between0and2 = Math.Abs(Rectangles[0].Width - Rectangles[2].Width);
+            betweenArry.Add(between0and2);
+            var between1and2 = Math.Abs(Rectangles[1].Width - Rectangles[2].Width);
+            betweenArry.Add(between1and2);
+            var minBetween = betweenArry.Min();
+            if (between0and1 == minBetween)
+            {
+                Rectangles.RemoveAt(2);
+            }
+            else if (between0and2 == minBetween)
+            {
+                Rectangles.RemoveAt(1);
+            }
+            else
+            {
+                Rectangles.RemoveAt(0);
+            }
+        }
+
         private static Bitmap GetUpdatedImage(Image<Bgr, byte> frame, List<Rectangle> Rectangles, Image<Bgr, byte> imageGlass)
         {
             Bitmap imageResult = new Bitmap(frame.Width, frame.Height);
             using (Graphics g = Graphics.FromImage(imageResult))
             {
-                RectangleF rect = new RectangleF(Rectangles[0].X, Rectangles[0].Y, Rectangles[1].X - Rectangles[0].X + Rectangles[1].Width, Rectangles[0].Height);
+                RectangleF rect = new RectangleF(Rectangles[0].X- Rectangles[0].Width/2, Rectangles[0].Y, Rectangles[1].X - Rectangles[0].X + Rectangles[1].Width+ Rectangles[0].Width / 2+ Rectangles[1].Width / 2, Rectangles[0].Height);
                 g.DrawImage(frame.Bitmap, 0, 0);
                 var glass = imageGlass.Bitmap;
                 glass.MakeTransparent();
@@ -140,7 +174,7 @@ namespace WeShow.Controllers
         }
         private Image<Bgr, Byte> ChooseGlass(string imagePath)
         {
-            CascadeClassifier haar = new CascadeClassifier("haarcascade_frontalface_default.xml");    //初始化分类器
+            CascadeClassifier haar = new CascadeClassifier(GetServerPath(@"Lib\haarcascade_frontalface_default.xml"));    //初始化分类器
             Image<Bgr, byte> frame = new Image<Bgr, byte>(imagePath);
             Rectangle[] results = haar.DetectMultiScale(frame, 1.3, 3, new System.Drawing.Size(10, 10));
             //检测并将数据储存
@@ -154,16 +188,16 @@ namespace WeShow.Controllers
                 if (brightness < 0.63)
                 {
                     ///黑皮肤
-                    return new Image<Bgr, byte>(GetServerPath(@"Libglass.png"));
+                    return new Image<Bgr, byte>(GetServerPath(@"Lib\glass.png"));
                 }
                 else
                 {
-                    return new Image<Bgr, byte>(GetServerPath(@"Libglass.png"));
+                    return new Image<Bgr, byte>(GetServerPath(@"Lib\glass.png"));
                 }
             }
             else
             {
-                return new Image<Bgr, byte>(GetServerPath(@"Libglass.png"));
+                return new Image<Bgr, byte>(GetServerPath(@"Lib\glass.png"));
             }
           
         
