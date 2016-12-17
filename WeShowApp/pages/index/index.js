3718c47668790116f2c16a67003be2bf4b40df3e
lily_cit <lilyjiang@ciandt.com>
2016-12-18 00:18:07 +0800

    Add tab feature

--- WeShowApp/pages/index/index.js
+++ WeShowApp/pages/index/index.js
@@ -1,23 +1,4 @@
-Page({
-  data: {
-    toView: 'red',
-    category: [{
-      id: '1',
-      name: '眼镜'
-    }, {
-      id: '2',
-      name: '帽子'
-    }, {
-      id: '3',
-      name: '上衣'
-    }, {
-      id: '4',
-      name: '裤子'
-    }, {
-      id: '5',
-      name: '连衣裙'
-    }],
-    glasses: [{
+var data = {glasses: [{
       id: '0101',
       src: '../../images/glasses/glasses0101.png'
     }, {
@@ -39,7 +20,57 @@ Page({
       id: '0206',
       src: '../../images/glasses/glasses0206.png'
     }],
-    showRightBar: ''
+    hats: [
+    //   {
+    //   id: '0101',
+    //   src: '../../images/hat/hat0101.png'
+    // }, {
+    //   id: '0201',
+    //   src: '../../images/hat/hat0201.png'
+    // }, 
+    {
+      id: '0301',
+      src: '../../images/hat/hat0301.png'
+    }, {
+      id: '0401',
+      src: '../../images/hat/hat0401.png'
+    }, {
+      id: '0501',
+      src: '../../images/hat/hat0501.png'
+    }, {
+      id: '0601',
+      src: '../../images/hat/hat0601.png'
+    }],
+    coats:[],
+    pants:[],
+    dresses:[]};
+Page({
+  data: {
+    toView: 'red',
+    categorys: [{
+      id: 'glasses',
+      name: '眼镜',
+      active:'active'
+    }, {
+      id: 'hats',
+      name: '帽子',
+      active:''
+    }, {
+      id: 'coats',
+      name: '上衣',
+      active:''
+    }, {
+      id: 'pants',
+      name: '裤子',
+      active:''
+    }, {
+      id: 'dresses',
+      name: '连衣裙',
+      active:''
+    }],
+    showRightBar: '',
+    sidebar: data.glasses,
+    selectedCategory: 'glasses'
   },
   upper: function(e) {
     console.log(e)
@@ -59,7 +90,26 @@ Page({
     console.log(e)
   },
   selectCategory: function(e) {
-    console.log(e);
+    var category = e.currentTarget.id,
+      categorys = this.data.categorys;
+
+    for (var i = 0, len = categorys.length; i < len; ++i) {
+      if (categorys[i].id == category) {
+        if(categorys[i].active == 'active'){
+          break;
+        }
+        categorys[i].active = 'active';
+      } else {
+        categorys[i].active = '';
+      }
+    }
+
+    this.setData({
+        sidebar: data[category],
+        selectedCategory: category,
+        showRightBar:'show',
+        categorys:categorys
+    });
   },
   toggleRightBar: function(e) {
       var showRightBar = (this.data.showRightBar ? '' : 'show');
