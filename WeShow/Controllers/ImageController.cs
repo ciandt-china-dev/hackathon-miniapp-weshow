fd1bd1901c0e84e44f2c759a0c22093fcd1cb530
dingsongjie <377973147@qq.com>
2016-12-17 16:45:30 +0800

    ff

--- WeShow/Controllers/ImageController.cs
+++ WeShow/Controllers/ImageController.cs
@@ -19,6 +19,7 @@ namespace WeShow.Controllers
     public class ImageController : ApiController
     {
         private string imageRoot = "imageTemp";
+        private string serverImageRoot;
         // GET: Image
         [HttpPost]
         [Route("image/automatic")]
@@ -26,26 +27,26 @@ namespace WeShow.Controllers
         {
             //文件保存目录路径 
             HttpPostedFile file = null;
-            if (!GetUploadImage(file))
+            if (!GetUploadImage(out file))
             {
                 return new JsonResult(new SampleResult() { Status = 2, Data = "上传文件出错" });
             }
-
+            serverImageRoot = GetServerPath(Path.Combine(imageRoot, file.FileName));
             CascadeClassifier haar = new CascadeClassifier(GetServerPath("Lib/haarcascade_eye.xml"));    //初始化分类器
 
-            Image<Bgr, byte> frame = new Image<Bgr, byte>(GetServerPath(Path.Combine(imageRoot, file.FileName)));  //加载上传的图片
+            Image<Bgr, byte> frame = new Image<Bgr, byte>(serverImageRoot);  //加载上传的图片
 
             List<Rectangle> Rectangles = GetEyesByImage(haar, frame);
 
 
-            Image<Bgr, Byte> imageGlass = new Image<Bgr, byte>(GetServerPath(@"Lib/glass.png"));
+            Image<Bgr, Byte> imageGlass = ChooseGlass(GetServerPath(serverImageRoot));
             if (Rectangles.Count != 2)
             {
                 return new JsonResult(new SampleResult() { Status = 2, Data = "未能正确识别人眼" });
             }
             SortRectangle(Rectangles);
             Bitmap imageResult = GetUpdatedImage(frame, Rectangles, imageGlass);
-            imageResult.Save(GetServerPath(Path.Combine(imageRoot, file.FileName)));
+            imageResult.Save(serverImageRoot);
 
             return new JsonResult(new SampleResult() { Status = 1, Data = Path.Combine("image", Path.Combine(imageRoot, file.FileName)) });
         }
@@ -90,11 +91,12 @@ namespace WeShow.Controllers
             return Rectangles;
         }
 
-        private bool GetUploadImage(HttpPostedFile file)
+        private bool GetUploadImage(out HttpPostedFile file)
         {
             var files = HttpContext.Current.Request.Files;
             if(files.Count!=1)
             {
+                file = null;
                 return false;
             }
             var f= files[0];
@@ -107,6 +109,65 @@ namespace WeShow.Controllers
         private string GetServerPath(string path)
         {
             return HttpContext.Current.Server.MapPath(path);
+        }
+        public double GetImageBrightness(Bitmap bitmap)
+        {
+            var colors = new List<Color>();
+            for (int x = 0; x < bitmap.Size.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Size.Height; y++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+                    var brightness = pixel.GetBrightness();
+                    if (brightness > 0.5 && brightness < 0.9)
+                        colors.Add(pixel);
+                }
+            }
+
+            return colors.Average(color => color.GetBrightness());
+        }
+
+        public Bitmap CovertRectangleToBitmap(Rectangle sourceRect, Bitmap image)
+        {
+            using (var bmp = new Bitmap((int)sourceRect.Width, (int)sourceRect.Height))
+            {
+                using (var graphics = Graphics.FromImage(bmp))
+                {
+                    graphics.DrawImage(image, 0.0f, 0.0f, sourceRect, GraphicsUnit.Pixel);
+                }
+                return bmp;
+            }
+        }
+        private Image<Bgr, Byte> ChooseGlass(string imagePath)
+        {
+            CascadeClassifier haar = new CascadeClassifier("haarcascade_frontalface_default.xml");    //初始化分类器
+            Image<Bgr, byte> frame = new Image<Bgr, byte>(imagePath);
+            Rectangle[] results = haar.DetectMultiScale(frame, 1.3, 3, new System.Drawing.Size(10, 10));
+            //检测并将数据储存
+            if(results.Count()>1)
+            {
+                Rectangle result = results[0];
+                     var image = frame.Bitmap;
+                CovertRectangleToBitmap(result, image);
+                var brightness = GetImageBrightness(image);
+
+                if (brightness < 0.63)
+                {
+                    ///黑皮肤
+                    return new Image<Bgr, byte>(GetServerPath(@"Libglass.png"));
+                }
+                else
+                {
+                    return new Image<Bgr, byte>(GetServerPath(@"Libglass.png"));
+                }
+            }
+            else
+            {
+                return new Image<Bgr, byte>(GetServerPath(@"Libglass.png"));
+            }
+          
+        
+            
         }
     }
 }
