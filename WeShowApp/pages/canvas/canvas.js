b051570a2e8d727458b53fd0461505fa8fe07807
l0m0l <l0m0l@qq.com>
2016-12-17 13:27:39 +0800

    tab

--- WeShowApp/pages/canvas/canvas.js
+++ WeShowApp/pages/canvas/canvas.js
@@ -1,25 +1,16 @@
 // pages/canvas/canvas.js
 Page({
-  data:{},
+  data:{
+
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
   onReady: function(e) {
     var context = wx.createContext()
     context.drawImage(
-      "pages/canvas/glass.png",0,0
+      "../../images/glass.png",0,0
     );
-    context.setFontSize(14)
-    context.fillText("MINA", 50, 50)
-    context.moveTo(0, 50)
-    context.lineTo(100, 50)
-    context.stroke()
-
-    context.setFontSize(20)
-    context.fillText("MINA", 100, 100)
-    context.moveTo(0, 100)
-    context.lineTo(200, 100)
-    context.stroke()
     wx.drawCanvas({
       canvasId: 1,
       actions: context.getActions()
