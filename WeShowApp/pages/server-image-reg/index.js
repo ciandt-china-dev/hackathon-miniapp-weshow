3c904744ac2f54e504264985e402dcdef9452a86
Jason Wang <jason@ciandt.com>
2016-12-18 02:54:26 +0800

    Update client upload image

--- WeShowApp/pages/server-image-reg/index.js
+++ WeShowApp/pages/server-image-reg/index.js
@@ -1,164 +1,133 @@
 Page({
-  data: {
-    toView: 'red',
-    modalHidden: true,
-    backgroundImage:'',
-    categorys: [{
-      id: '1',
-      key:'glass',
-      name: '眼镜'
-    }, {
-      id: '2',
-      key:'hat',
-      name: '帽子'
-    }, {
-      id: '3',
-      key:'coat',
-      name: '上衣'
-    }, {
-      id: '4',
-      key:'pants',
-      name: '裤子'
-    }, {
-      id: '5',
-      key:'dress',
-      name: '连衣裙'
-    }],
-    glasses: [{
-      id: '0101',
-      src: '../../images/glasses/glasses0101.png'
-    }, {
-      id: '0201',
-      src: '../../images/glasses/glasses0201.png'
-    }, {
-      id: '0202',
-      src: '../../images/glasses/glasses0202.png'
-    }, {
-      id: '0203',
-      src: '../../images/glasses/glasses0203.png'
-    }, {
-      id: '0204',
-      src: '../../images/glasses/glasses0204.png'
-    }, {
-      id: '0205',
-      src: '../../images/glasses/glasses0205.png'
-    }, {
-      id: '0206',
-      src: '../../images/glasses/glasses0206.png'
-    }],
-    //showRightBar: ''
-  },
-  onLoad: function(options){
-    this.changeBackGroundImage()
-  },
-  upper: function(e) {
-    console.log(e)
-  },
-  lower: function(e) {
-    console.log(e)
-  },
-  scroll: function(e) {
-    console.log(e)
-  },
-  scrollToTop: function(e) {
-    this.setAction({
-      scrollTop: 0
-    })
-  },
-  modalChange: function(e) {
-    this.setData({
-      modalHidden: true
-    })
-  },
-  selectGoods: function(e) {
-    console.log(e)
-  },
-  selectCategory: function(e) {
-    var category = e.currentTarget.id,
-      categorys = this.data.categorys;
-    console.log(categorys);
-    for (var i = 0, len = categorys.length; i < len; ++i) {
-      if (categorys[i].id == category) {
-        if(categorys[i].active == 'active'){
-          categorys[i].active = '';
-        }else{
-          categorys[i].active = 'active';
-        }
-        break;
-      }
-    }
-
-    this.setData({
-        //sidebar: data[category],
-        //selectedCategory: category,
-        //showRightBar:'show',
-        categorys:categorys
-    });
-  },
-  // toggleRightBar: function(e) {
-  //   var showRightBar = (this.data.showRightBar ? '' : 'show');
-  //   this.setData({
-  //     showRightBar: showRightBar
-  //   });
-  // },
-  changeBackGroundImage:function(){
-      var that = this;
-      wx.showModal({
-          title: '微秀',
-          content: that.backgroundImage?'换一张图片？':'请选择一张照片，一起微秀',
-          success: function(res) {
-              if(res.confirm)
-                wx.chooseImage({
-                  count: 1, // 最多可以选择的图片张数，默认9
-                  sizeType: ['compressed'],
-                  sourceType: ['album', 'camera'],
-                  complete: function(e) {
-                    if(!e.tempFilePaths){
-                        return;
-                    }
-                    that.backgroundImage = e.tempFilePaths[0];
+    data: {
+        toView: 'red',
+        backgroundImage: '',
+        categorys: [{
+            id: '1',
+            optionkey: 'glass',
+            name: '眼镜'
+        }, {
+            id: '2',
+            optionkey: 'hat',
+            name: '帽子'
+        }, {
+            id: '3',
+            optionkey: 'coat',
+            name: '上衣'
+        }, {
+            id: '4',
+            optionkey: 'pants',
+            name: '裤子'
+        }, {
+            id: '5',
+            optionkey: 'dress',
+            name: '连衣裙'
+        }],
+    },
+    onLoad: function(options) {
+        this.changeBackGroundImage()
+    },
+    upper: function(e) {
+        console.log(e)
+    },
+    lower: function(e) {
+        console.log(e)
+    },
+    scroll: function(e) {
+        console.log(e)
+    },
+    scrollToTop: function(e) {
+        this.setAction({
+            scrollTop: 0
+        })
+    },
+    selectGoods: function(e) {
+        console.log(e)
+    },
+    selectCategory: function(e) {
+        var category = e.currentTarget.id,
+            categorys = this.data.categorys;
+        //console.log(categorys);
+        for (var i = 0, len = categorys.length; i < len; ++i) {
+            if (categorys[i].id == category) {
+                if (categorys[i].active == 'active') {
+                    categorys[i].active = '';
+                } else {
+                    categorys[i].active = 'active';
                 }
-             })
-           }
+                break;
+            }
+        }
+        this.setData({
+            categorys: categorys,
         });
-  },
-  chooseImage: function(e){
-    var that = this
-    wx.chooseImage({
-      count: 1, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function success(res) {
-        wx.showToast({
-          title: '图片上传中',
-          icon: 'loading',
-          duration: 10000
-        })
+    },
+    // toggleRightBar: function(e) {
+    //   var showRightBar = (this.data.showRightBar ? '' : 'show');
+    //   this.setData({
+    //     showRightBar: showRightBar
+    //   });
+    // },
+    changeBackGroundImage: function() {
+        var that = this;
+        wx.showModal({
+            title: '微秀',
+            content: that.backgroundImage ? '换一张图片？' : '请选择一张照片，一起微秀',
+            success: function(res) {
+                if (res.confirm)
+                    wx.chooseImage({
+                        count: 1, // 最多可以选择的图片张数，默认9
+                        sizeType: ['compressed'],
+                        sourceType: ['album', 'camera'],
+                        complete: function(e) {
+                            if (!e.tempFilePaths) {
+                                return;
+                            }
+                            that.setData({
+                                backgroundImage: e.tempFilePaths[0],
+                            })
+                        }
+                    })
+            }
+        });
+    },
+    chooseImage: function(e) {
+        var that = this
+        wx.chooseImage({
+            count: 1, // 默认9
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function success(res) {
+                wx.showToast({
+                    title: '图片上传中',
+                    icon: 'loading',
+                    duration: 10000
+                })
 
-        setTimeout(function(){
-          wx.hideToast()
-        },10000)
-        wx.uploadFile({
-          url: 'https://weshow.nbhanyi.com/glass,hat', //仅为示例，非真实的接口地址
-          filePath: res.tempFilePaths[0],
-          name: 'file',
-          formData:{},
-          success: function(res){
-            var filePath = 'https://weshow.nbhanyi.com/' + res.data
-            // that.setData({
-            //   tempFilePath: filePath
-            // })
-            console.log(filePath)
-            wx.previewImage({
-              current: filePath, // 当前显示图片的http链接
-              urls: [filePath] // 需要预览的图片http链接列表
-            })
-            wx.hideToast()
-          }
+                setTimeout(function() {
+                    wx.hideToast()
+                }, 10000)
+                wx.uploadFile({
+                    url: 'https://weshow.nbhanyi.com/glass,hat', //仅为示例，非真实的接口地址
+                    filePath: res.tempFilePaths[0],
+                    name: 'file',
+                    formData: {},
+                    success: function(res) {
+                        var filePath = 'https://weshow.nbhanyi.com/' + res.data
+                            // that.setData({
+                            //   tempFilePath: filePath
+                            // })
+                        console.log(filePath)
+                        wx.previewImage({
+                            current: filePath, // 当前显示图片的http链接
+                            urls: [filePath] // 需要预览的图片http链接列表
+                        })
+                        wx.hideToast()
+                    }
+                })
+            }
         })
-      }
-    })
-  }
+    }
 })
 
 // //index.js
