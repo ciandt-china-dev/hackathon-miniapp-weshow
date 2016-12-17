cf512eac64f9b6aacc40ae9426c057a78a6e0b9b
lily_cit <lilyjiang@ciandt.com>
2016-12-17 18:59:48 +0800

    Add server page

--- WeShowApp/pages/server-image-reg/index.js
+++ WeShowApp/pages/server-image-reg/index.js
@@ -1,26 +1,97 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    toView: 'red',
+    category: [{
+      id: '1',
+      name: '眼镜'
+    }, {
+      id: '2',
+      name: '帽子'
+    }, {
+      id: '3',
+      name: '上衣'
+    }, {
+      id: '4',
+      name: '裤子'
+    }, {
+      id: '5',
+      name: '连衣裙'
+    }],
+    glasses: [{
+      id: '0101',
+      src: '../../images/glasses/glasses0101.png'
+    }, {
+      id: '0201',
+      src: '../../images/glasses/glasses0201.png'
+    }, {
+      id: '0202',
+      src: '../../images/glasses/glasses0202.png'
+    }, {
+      id: '0203',
+      src: '../../images/glasses/glasses0203.png'
+    }, {
+      id: '0204',
+      src: '../../images/glasses/glasses0204.png'
+    }, {
+      id: '0205',
+      src: '../../images/glasses/glasses0205.png'
+    }, {
+      id: '0206',
+      src: '../../images/glasses/glasses0206.png'
+    }],
+    showRightBar: ''
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  upper: function(e) {
+    console.log(e)
+  },
+  lower: function(e) {
+    console.log(e)
+  },
+  scroll: function(e) {
+    console.log(e)
+  },
+  scrollToTop: function(e) {
+    this.setAction({
+      scrollTop: 0
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+  selectGoods: function(e) {
+    console.log(e)
+  },
+  selectCategory: function(e) {
+    console.log(e);
+  },
+  toggleRightBar: function(e) {
+    var showRightBar = (this.data.showRightBar ? '' : 'show');
+    this.setData({
+      showRightBar: showRightBar
+    });
   }
 })
+
+// //index.js
+// //获取应用实例
+// var app = getApp()
+// Page({
+//   data: {
+//     motto: 'Hello World',
+//     userInfo: {}
+//   },
+//   //事件处理函数
+//   bindViewTap: function() {
+//     wx.navigateTo({
+//       url: '../logs/logs'
+//     })
+//   },
+//   onLoad: function () {
+//     console.log('onLoad')
+//     var that = this
+//     //调用应用实例的方法获取全局数据
+//     app.getUserInfo(function(userInfo){
+//       //更新数据
+//       that.setData({
+//         userInfo:userInfo
+//       })
+//     })
+//   }
+// })
