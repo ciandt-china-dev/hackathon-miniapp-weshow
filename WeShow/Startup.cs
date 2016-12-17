7b178f74b97ed87a2cea13f5500a91a2d2a9b70d
Jason Wang <jason@ciandt.com>
2016-12-17 20:56:31 +0800

    Remove unused dlls

--- WeShow/Startup.cs
+++ WeShow/Startup.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartup(typeof(WeShow.Startup))]
+//[assembly: OwinStartup(typeof(WeShow.Startup))]
 
 namespace WeShow
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-           // ConfigureAuth(app);
-        }
+        //public void Configuration(IAppBuilder app)
+        //{
+        //   // ConfigureAuth(app);
+        //}
     }
 }
